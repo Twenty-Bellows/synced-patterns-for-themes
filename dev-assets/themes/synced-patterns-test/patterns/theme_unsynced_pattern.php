14e8cefdefbdeeebc7970c16a18a76650369924c
Jason Crist <jcrist@pbking.com>
2025-05-02 17:59:53 -0400

    Added a test theme to develop with. Cleaned up some other environment setup.

---
+++ dev-assets/themes/synced-patterns-test/patterns/theme_unsynced_pattern.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Title: Theme Unsynced Pattern
+ * Slug: synced-patterns-test/theme-unsynced-pattern
+ * Categories: text
+ * Description: A group block with a blue background and text.
+ */
+
+?>
+<!-- wp:group {"style":{"color":{"background":"#d6cfff"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group has-background" style="background-color:#d6cfff"><!-- wp:heading {"level":1} -->
+<h1 class="wp-block-heading">This is a Theme SYNCED Pattern</h1>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
