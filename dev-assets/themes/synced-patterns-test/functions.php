4225b740ab689bad07fb51a867200c7e37864d12
Jason Crist <jcrist@pbking.com>
2025-05-05 19:01:26 -0400

    Simplified sample theme

---
+++ dev-assets/themes/synced-patterns-test/functions.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * 
+ * Remove core block patterns
+ * 
+ */
+add_action('after_setup_theme', function () {
+	remove_theme_support('core-block-patterns');
+});
+
--- dev-assets/themes/synced-patterns-test/patterns/theme_unsynced_pattern.php
+++ dev-assets/themes/synced-patterns-test/patterns/theme_unsynced_pattern.php
@@ -9,6 +9,6 @@
 ?>
 <!-- wp:group {"style":{"color":{"background":"#d6cfff"}},"layout":{"type":"default"}} -->
 <div class="wp-block-group has-background" style="background-color:#d6cfff"><!-- wp:heading {"level":1} -->
-<h1 class="wp-block-heading">This is a Theme SYNCED Pattern</h1>
+<h1 class="wp-block-heading">This is a Theme UNSYNCED Pattern</h1>
 <!-- /wp:heading --></div>
 <!-- /wp:group -->
